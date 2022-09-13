245e714deb06f5ba27759a30a684b51e55da2b2b
Cam1lo <camilojosecnc@gmail.com>
2022-09-13 16:28:14 -0400

    Adding options to gitignore

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,7 @@
 import axios from "axios";
-
+import { options } from "../options";
 
 export default async function get_coins() {
-    const options = {
-        method: 'GET',
-        url: 'https://coinranking1.p.rapidapi.com/coins',
-        params: {
-            referenceCurrencyUuid: 'yhjMzLPhuIDl',
-            timePeriod: '24h',
-            'tiers[0]': '1',
-            orderBy: 'marketCap',
-            orderDirection: 'desc',
-            limit: '50',
-            offset: '0'
-        },
-        headers: {
-            'X-RapidAPI-Key': '583706893fmshe101a6929f7a63fp1083bcjsneea8e8466986',
-            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-        }
-    };
-
     let data;
 
     await axios.request(options).then(function (response) {
