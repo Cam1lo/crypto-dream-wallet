03ca35d004d13c2d2b1e43cf23236f37ad8affed
Cam1lo <camilojosecnc@gmail.com>
2023-02-06 11:47:13 -0500

    NavBar and Points layout fixed

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,39 +1,51 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Market from '../market/Market';
-import GetMoreCoins from './get-more-coins/GetMoreCoins';
-import NavBar from '../nav-bar/NavBar';
-import Portfolio from '../portfolio/Portfolio';
-import React from 'react';
-import { useConfig } from '../../services/CustomHooks';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Market from "../market/Market";
+import GetMoreCoins from "./get-more-coins/GetMoreCoins";
+import NavBar from "../nav-bar/NavBar";
+import Portfolio from "../portfolio/Portfolio";
+import React from "react";
+import { useConfig } from "../../services/CustomHooks";
 
 function App() {
-  const [active_page, set_active_page] = useState<string>('portfolio')
+  const [active_page, set_active_page] = useState<string>("portfolio");
   const loading = useConfig();
-  
+
   const switch_screen = (pagename: string) => {
-    set_active_page(pagename)
-  }
+    set_active_page(pagename);
+  };
 
   function active_page_fun() {
-    if (active_page === 'portfolio') {
-      return <Portfolio goToMarket={() => {
-        switch_screen('market')
-      }} />
-    } if (active_page === 'market') {
-      return <Market />
+    if (active_page === "portfolio") {
+      return (
+        <Portfolio
+          goToMarket={() => {
+            switch_screen("market");
+          }}
+        />
+      );
+    }
+    if (active_page === "market") {
+      return <Market />;
     }
   }
 
   return (
     <>
-      {loading ? <div>LOADING</div> :
-        <div className="App">
-          <GetMoreCoins/>
-          <NavBar switch_function={switch_screen} active_page_name={active_page} />
-          {active_page_fun()}
+      {loading ? (
+        <div>LOADING</div>
+      ) : (
+        <div className="App-wrapper">
+          <GetMoreCoins />
+          <div className="App">
+            <NavBar
+              switch_function={switch_screen}
+              active_page_name={active_page}
+            />
+            {active_page_fun()}
+          </div>
         </div>
-      }
+      )}
     </>
   );
 }
