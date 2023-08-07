e4b345f3d86ab34287a86adbc529df56d7eb7626
Cam1lo <47703795+Cam1lo@users.noreply.github.com>
2023-08-07 01:11:22 -0400

    build fixes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Market from "../market/Market";
 import GetMoreCoins from "./get-more-coins/GetMoreCoins";
